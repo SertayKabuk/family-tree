bd570bf19d40dc5f77ad5efbee3505c41b5d6945
Sertay Kabuk <sertaykabuk@gmail.com>
2025-12-27 02:16:07 +0300

    feat: change default locale from English to Turkish

--- i18n/config.ts
+++ i18n/config.ts
@@ -1,7 +1,7 @@
 export const locales = ["en", "tr"] as const;
 export type Locale = (typeof locales)[number];
 
-export const defaultLocale: Locale = "en";
+export const defaultLocale: Locale = "tr";
 
 export const localeNames: Record<Locale, string> = {
   en: "English",
